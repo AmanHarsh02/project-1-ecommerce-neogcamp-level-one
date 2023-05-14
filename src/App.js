73718effa8f83c475e2fd2155f5ce0b0d454f175
Aman Harsh <amanharsh0399@gmail.com>
2023-05-15 01:10:22 +0530

    update in db: added custom data in database
    Updated initial database files for products, categories and users and added custom data to these files and tested Api using mockman

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Mockman from "mockman-js";
 
 function App() {
+  console.log(process.env.REACT_APP_JWT_SECRET, "ENV");
   return (
     <div className="App">
       <h1>E-Commerce Project</h1>
+      <Mockman />
     </div>
   );
 }
--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -9,10 +9,10 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    email: "adarshbalika@gmail.com",
-    password: "adarshbalika",
+    firstName: "Aman",
+    lastName: "Harsh",
+    email: "amanharsh0399@gmail.com",
+    password: "amanharsh",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
